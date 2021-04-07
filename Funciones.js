39da8beed70d03a314adeb9ca13f9ff36ade6263
LuisRojaszZ <rojaszavalaj@gmail.com>
2021-04-07 02:47:47 -0500

    sintactico

--- Funciones.js
+++ Funciones.js
@@ -6,24 +6,35 @@
 	var array = expresion.split("");
 	for (var i in array) 
 	{
-		switch (true) 
-		{
-			case /[a-z]/.test(array[i]):
-			resultado.innerHTML = resultado.innerHTML + array[i] + " Es una letra\n";
-			break;
 
-			case /[0-9]/.test(array[i]):
-			resultado.innerHTML = resultado.innerHTML + array[i] + "  Es un numero\n";
-			break;
-
-			case /[+]|[*]|[/]|[()]|[-]|[=]/.test(array[i]): 
-			resultado.innerHTML = resultado.innerHTML + array[i] + "Es un Operador\n";
-
-			case /^([(0-9])?[+\-*\/0-9]+([+*\-\/)])?[+*\-\/0-9]+([=])[0-9]+$/.test(array[i])||/^([0-9][*\-\/+])+([0-9])+/.test(array[i])||/^[0-9][+\/\-*]+[0-9]+$/.test(array[i]):
-			resultado2.innerHTML=resultado2.innerHTML+array[i]+"La cadena esa escrita corresctamente";
-			break;
-
-		}
+	    if(/[a-z]/.test(array[i]))
+	    {
+	        resultado.innerHTML = resultado.innerHTML + array[i] + " :Este elemento es una letra\n";
+        }
+	    if(/[0-9]/.test(array[i]))
+        {
+            resultado.innerHTML = resultado.innerHTML + array[i] + " :Este elemento en un numero\n"
+        }
+	    if (/[+]|[*]|[/]|[()]|[-]|[=]/.test(array[i]))
+	    {
+	        resultado.innerHTML = resultado.innerHTML + array[i] + " :Este elemento es un operador\n"
+        }
 	}
+        document.getElementById("val2").onclick=function()
+        {
+            var expresion = document.getElementById("val2").value;
+            var resultado = document.getElementById("ResultadoSintactica");
+            var array2=expresion.split(" ");
+            for (var j in array2)
+            {
 
+                if (/^([(0-9])?[+\-*\/0-9]+([+*\-\/)])?[+*\-\/0-9]+([=])[0-9]+$/.test(array2[j])||/^([0-9][*\-\/+])+([0-9])+/.test(array2[j])||/^[0-9][+\/\-*]+[0-9]+$/.test(array2[j]))
+                {
+                    resultado2.innerHTML= resultado2.innerHTML+ array2[j]+" :La cadena esta correctamente escrita\n";
+                }
+                else {
+                    resultado2.innerHTML= resultado2.innerHTML+ array2[j]+" :La cadena esta escrita Incorrectamente escrita\n";
+                }
+            }
+        }
 }
